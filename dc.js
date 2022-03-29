2a06e742c5e5d52f89997424024af6761e6a61a7
Matheus Ribeiro <59946545+MgRibeir0@users.noreply.github.com>
2022-03-29 18:14:37 -0300

    Now the Successful registered message just appears when it happens

--- dc.js
+++ dc.js
@@ -1,18 +1,24 @@
 const fs = require('fs')
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
+const { clientID, guildID, token } = require('./config.json');
 
 const commands = [];
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
-    commands.push(command);
-    console.log(`[${new Date().toLocaleString()}] [INFO] Registered command: ${command.data.name}`);
+    commands.push(command.data.toJSON());
+    console.log(`[${new Date().toLocaleString()}] [INFO] Loaded command: ${command.data.name}`);
 }
 
 const rest = new REST({ version: '9' }).setToken(token);
 
 rest.put(Routes.applicationGuildCommands(clientID, guildID), { body: commands })
-    .then(() => console.log(`[${new Date().toLocaleString()}] [INFO] Successfully registered commands`))
+    .then(() => {
+        for (const i in commandFiles) {
+            console.log(`[${new Date().toLocaleString()}] [SUCCESS] Registered command: ${commandFiles[i]}`);
+        }
+        console.log(`[${new Date().toLocaleString()}] [SUCCESS] Successfully registered commands`)
+    })
     .catch(console.error);
